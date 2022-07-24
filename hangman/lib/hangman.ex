b36f68f5734ef1869adab90a3eb54f2b5453e970
Benjamin <benjamin.guy.thomas@gmail.com>
2022-07-24 10:22:34 +0200

    Extract types to module

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,19 +1,11 @@
 defmodule Hangman do
   alias Hangman.Impl
   @opaque game :: Impl.Game.t()
-  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
-
-  @type tally :: %{
-          turns_left: integer,
-          game_state: State.state(),
-          letters: list(String.t()),
-          used: list(String.t())
-        }
 
   @spec new_game :: game
   defdelegate new_game, to: Impl.Game
 
-  @spec make_move(game, String.t()) :: {game, tally}
+  @spec make_move(game, String.t()) :: {game, Type.tally()}
   def make_move(_game, _guess) do
   end
 end
