5d6e5b3c72be439d318c09fa3ca3fed36ae56925
Benjamin <benjamin.guy.thomas@gmail.com>
2022-08-07 09:23:31 +0200

    Delegate to an "application"

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,6 @@
 defmodule Dictionary do
   alias Dictionary.Runtime.Server
-  @opaque t :: Server.t()
 
-  @spec start_link :: {:error, any} | {:ok, pid}
-  defdelegate start_link, to: Server
-
-  @spec random_word(t()) :: String.t()
-  defdelegate random_word(words), to: Server
+  @spec random_word() :: String.t()
+  defdelegate random_word(), to: Server
 end
