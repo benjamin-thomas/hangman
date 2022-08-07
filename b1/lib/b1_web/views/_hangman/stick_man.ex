b38dd3195a582b5cde5fbc254f8e5c157268ff9b
Benjamin <benjamin.guy.thomas@gmail.com>
2022-08-07 21:17:02 +0200

    Cleanup

---
+++ b1/lib/b1_web/views/_hangman/stick_man.ex
@@ -0,0 +1,98 @@
+defmodule B1Web.Hangman.StickMan do
+  @spec stick_man(0 | 1 | 2 | 3 | 4 | 5 | 6 | 7) :: String.t()
+  def stick_man(0) do
+    ~S{
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    =========
+    }
+  end
+
+  def stick_man(1) do
+    ~S{
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+
+     =========
+     }
+  end
+
+  def stick_man(2) do
+    ~S{
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========
+    }
+  end
+
+  def stick_man(3) do
+    ~S{
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    }
+  end
+
+  def stick_man(4) do
+    ~S{
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
+    }
+  end
+
+  def stick_man(5) do
+    ~S{
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    }
+  end
+
+  def stick_man(6) do
+    ~S{
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    }
+  end
+
+  def stick_man(7) do
+    ~S{
+      +---+
+          |
+          |
+          |
+          |
+          |
+    =========
+    }
+  end
+end
