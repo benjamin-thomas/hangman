1128faf6069e11a3077febc93a9799220c11149e
Benjamin <benjamin.guy.thomas@gmail.com>
2022-08-07 11:37:28 +0200

    Implement GenServer calls

---
+++ hangman/lib/runtime/server.ex
@@ -0,0 +1,25 @@
+defmodule Hangman.Runtime.Server do
+  alias Hangman.Impl.Game
+  use GenServer
+
+  ### client processes
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+  ### server processes
+
+  def init(_) do
+    {:ok, Game.new_game()}
+  end
+
+  def handle_call({:make_move, guess}, _from, game) do
+    {updated_ame, tally} = Game.make_move(game, guess)
+    {:reply, tally, updated_ame}
+  end
+
+  def handle_call({:tally}, _from, game) do
+    {:reply, Game.tally(game), game}
+  end
+end
